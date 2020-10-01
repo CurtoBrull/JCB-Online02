cbddb30cb67f109b098bfebd59af4322334eb380
Javier Curto <curto.brull.javier@gmail.com>
2020-10-01 22:50:45 +0200

    hasta E8

--- src/jcb/online02/E8_JCB.java
+++ src/jcb/online02/E8_JCB.java
@@ -4,28 +4,17 @@ import java.util.Scanner;
 public class E8_JCB {
 
     public static void main(String[] args) {
-        /* 
-        Muestre la tabla de multiplicar de un número introducido por teclado.
-        */
         Scanner teclado = new Scanner(System.in);
-        System.out.print("Introducir el número: ");        
-        int numero = teclado.nextInt();
-        /*
-        Empieza la variable a iterar (i) desde 0; mientras i sea menor a 10;
-        sumamos 1 a i
-        */
+        String r = teclado.nextLine();
         do {
+            Scanner teclado2 = new Scanner(System.in);
+            System.out.print("Introducir el número: ");        
+            int numero = teclado2.nextInt();              
             for(int i=0; i<11; i++) {
-            System.out.println(numero + " * " + i + " = " + (numero * i));            
+                System.out.println(numero + " * " + i + " = " + (numero * i));
             }
-        }while (respuesta == "s" | respuesta == "S");
-        
-        Scanner teclado2 = new Scanner(System.in);
-        System.out.println("Desea ver otra tabla?(s/n): ");
-        String respuesta;
-        respuesta = teclado2.nextLine();        
-        System.out.println(":::::::...FIN...:::::::");
-
+        } while (r == "s");
+         
     }
     
 }      
